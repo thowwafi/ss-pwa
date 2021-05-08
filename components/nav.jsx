4e3d2dfbde28f7a1827a252181da994ad5abb5e8
Thowwafi <thowwafi@gmail.com>
2021-05-08 08:22:01 +0700

    remove h full screen

--- components/nav.jsx
+++ components/nav.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Nav() {
   return (
-    <div className="w-full h-screen">
+    <div className="w-full">
       <section className="block fixed inset-x-0 bottom-0 z-10 bg-white shadow">
         <div className="flex justify-between">
           <Link href={'/'}>
